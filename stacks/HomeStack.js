8309c2fbff5d6137674a151cfc1698c9e7942bb6
GiaHuyIUH <tranhuyzaza@gmail.com>
2024-11-21 22:54:38 +0700

    Modify HomeStack and SearchStack

--- stacks/HomeStack.js
+++ stacks/HomeStack.js
@@ -2,6 +2,11 @@ import { View } from "react-native";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
+import ListeningScreen from "../screens/ListeningScreen";
+import TrackViewScreen from "../screens/TrackViewScreen";
+import TrackControlScreen from "../screens/TrackControlScreen";
+import SongShareScreen from "../screens/SongShareScreen";
+import PlayList from "../modules/Playlist/Playlist";
 // import PlayList from "../modules/Playlist/PlayList";
 const Stack = createNativeStackNavigator();
 export default function HomeStack() {
@@ -12,25 +17,36 @@ export default function HomeStack() {
         component={HomeScreen}
         options={{ headerShown: false }}
       />
-      {/* <Stack.Screen
-                name="PlayList"
-                component={PlayList}
-                options={{
-                    headerTitle: "",
-                    headerBackground: () => {
-                        return (
-                            <View
-                                style={{
-                                    background: "#0080AE",
-                                    flex: 1,
-                                }}
-                            ></View>
-                        );
-                    },
-                    headerTintColor: "white",
-                    headerShadowVisible: false,
-                }}
-            /> */}
+      <Stack.Screen
+        name="TrackControlScreen"
+        component={TrackControlScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SongShareScreen"
+        component={SongShareScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="PlayList"
+        component={PlayList}
+        options={{
+          headerTitle: "My Playlist",
+          headerBackground: () => (
+            <View
+              style={{
+                backgroundColor: "#121212",
+                flex: 1,
+              }}
+            />
+          ),
+          headerTintColor: "white",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerShadowVisible: false,
+        }}
+      />
     </Stack.Navigator>
   );
 }
